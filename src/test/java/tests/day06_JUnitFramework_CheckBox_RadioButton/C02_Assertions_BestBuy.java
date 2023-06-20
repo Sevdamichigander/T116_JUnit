fdf4c2cc20ca88ebffa28aa9f2cb413cd64f7bfa
Sevda BAGCI KARATAS <sevdabagcikaratas@gmail.com>
2023-06-20 11:46:54 +0200

    Youtube test

--- src/test/java/tests/day06_JUnitFramework_CheckBox_RadioButton/C02_Assertions_BestBuy.java
+++ src/test/java/tests/day06_JUnitFramework_CheckBox_RadioButton/C02_Assertions_BestBuy.java
@@ -5,6 +5,7 @@ import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -21,6 +22,8 @@ public class C02_Assertions_BestBuy {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.get("https://www.bestbuy.com/");
+
     }
     @AfterClass
     public static void tearDown(){
@@ -31,23 +34,40 @@ public class C02_Assertions_BestBuy {
     // Sayfa URL’inin https://www.bestbuy.com/ ’a esit oldugunu test edin
 
     @Test
-    public void test01(){
-        driver.get("https://www.bestbuy.com/");
+    public void urlTest(){
+
+        String expectedUrl = "https://www.bestbuy.com/";
 
         String actualUrl = driver.getCurrentUrl();
 
-        Assert.assertEquals("https://www.bestbuy.com/",(actualUrl));
+        Assert.assertEquals(expectedUrl,actualUrl);
 
     }
     @Test
-    public void test02(){
+    public void titleTest(){
         // titleTest => Sayfa başlığının “Rest” içermediğini(contains) test edin
 
-        String expectedTitleContains = "Rest";
+        String unexpectedTitleContains = "Rest";
         String actualTitle = driver.getTitle();
 
-        Assert.assertTrue(expectedTitleContains, Boolean.parseBoolean(actualTitle));
+        Assert.assertFalse(actualTitle.contains(unexpectedTitleContains));
 
+    }
+
+    @Test
+    public void logoTest(){
+
+        //BestBuy logosunun görüntülendigini test edin
+        WebElement logo = driver.findElement(By.xpath("//a[@title='BestBuy.com']"));
+
+        Assert.assertTrue(logo.isDisplayed());
+    }
+
+    @Test
+    public void francaisLinkTest() {
+
+        WebElement francaisLink = driver.findElement(By.xpath("//button[@lang='fr']"));
+        Assert.assertTrue(francaisLink.isDisplayed());
     }
 
 
