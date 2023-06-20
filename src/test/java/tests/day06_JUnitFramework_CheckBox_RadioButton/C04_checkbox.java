36b659a23cc6fc41196baac15c90c3640e4012b8
Sevda BAGCI KARATAS <sevdabagcikaratas@gmail.com>
2023-06-20 11:50:28 +0200

    checkbox

---
+++ src/test/java/tests/day06_JUnitFramework_CheckBox_RadioButton/C04_checkbox.java
@@ -0,0 +1,15 @@
+package tests.day06_JUnitFramework_CheckBox_RadioButton;
+
+public class C04_checkbox {
+
+}
+
+/**
+ * Gerekli yapiyi olusturun ve aşağıdaki görevi tamamlayın.
+ *  a. Verilen web sayfasına gidin.
+ *      https://the-internet.herokuapp.com/checkboxes
+ *  b. Checkbox1 ve checkbox2 elementlerini locate edin.
+ *  c. Checkbox1 seçili değilse onay kutusunu tıklayın
+ *  d. Checkbox2 seçili değilse onay kutusunu tıklayın
+ *  e. Checkbox1ve Checkbox2’nin seçili olduğunu test edin
+ */
