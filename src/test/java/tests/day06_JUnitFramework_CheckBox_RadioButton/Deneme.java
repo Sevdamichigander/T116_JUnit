0e72792506c2ec9da2f930155e8e50b89c045158
Sevda BAGCI KARATAS <sevdabagcikaratas@gmail.com>
2023-07-04 10:13:26 +0200

    explicitly wait

--- src/test/java/tests/day06_JUnitFramework_CheckBox_RadioButton/Deneme.java
+++ src/test/java/tests/day06_JUnitFramework_CheckBox_RadioButton/Deneme.java
@@ -1,17 +1,31 @@
 package tests.day06_JUnitFramework_CheckBox_RadioButton;
 
-public class Deneme {
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utilities.TestBase;
+
+public class Deneme extends TestBase {
+    @Test
+    public void test01() {
+        // Gerekli yapiyi olusturun ve aşağıdaki görevi tamamlayın.
+        //         a. Verilen web sayfasına gidin.
+        //         https://facebook.com
+        driver.get("https://facebook.com");
+        // b. Cookies’i kabul edin
+        // c. Create an account buton’una basin
+        // d. Radio button elementlerini locate edin ve size uygun olani secin
+        // e. Sectiginiz radio button’un seçili, ötekilerin seçili olmadigini test edin
+
+
+
+    }
 }
-/*
 
 
-        Gerekli yapiyi olusturun ve aşağıdaki görevi tamamlayın.
-        a. Verilen web sayfasına gidin.
-        https://the-internet.herokuapp.com/checkboxes
-        b. Checkbox1 ve checkbox2 elementlerini locate edin.
-        c. Checkbox1 seçili değilse onay kutusunu tıklayın
-        d. Checkbox2 seçili değilse onay kutusunu tıklayın
-        e. Checkbox1ve Checkbox2’nin seçili olduğunu test edin
 
-        */
+
+
+
 
